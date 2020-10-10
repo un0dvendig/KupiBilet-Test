8f83213e862ffab7aca9305a8c0397b54d699561
Eugene Ilyin <evgen.ilyin@gmail.com>
2020-10-10 11:03:16 +0300

    Initial Commit

---
+++ KupiBilet Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KupiBilet Test
+//
+//  Created by Eugene Ilyin on 10.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
